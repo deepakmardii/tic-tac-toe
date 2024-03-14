0f7561b56a4168a0420aba1e2cd0aa1d00fd20d7
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-14 19:03:46 +0530

    click handler and current state

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,29 @@ import { useState } from "react";
 import Block from "./components/Block";
 function App() {
   const [state, setState] = useState(Array(9).fill(null));
-  console.log("state", state);
+  const [curentTurn, setCurrentTurn] = useState("X");
+
+  const handleBlockClick = (index: number) => {
+    console.log(index);
+  };
+  // console.log("state", state);
   return (
     <>
       <div className="p-20 ">
         <div className="flex">
-          <Block value="x" />
-          <Block value="o" />
-          <Block value="x" />
+          <Block onClick={() => handleBlockClick(0)} value={state[0]} />
+          <Block onClick={() => handleBlockClick(1)} value={state[1]} />
+          <Block onClick={() => handleBlockClick(2)} value={state[2]} />
         </div>
         <div className="flex">
-          <Block />
-          <Block />
-          <Block />
+          <Block onClick={() => handleBlockClick(3)} value={state[3]} />
+          <Block onClick={() => handleBlockClick(4)} value={state[4]} />
+          <Block onClick={() => handleBlockClick(5)} value={state[5]} />
         </div>
         <div className="flex">
-          <Block />
-          <Block />
-          <Block />
+          <Block onClick={() => handleBlockClick(6)} value={state[6]} />
+          <Block onClick={() => handleBlockClick(7)} value={state[7]} />
+          <Block onClick={() => handleBlockClick(8)} value={state[8]} />
         </div>
       </div>
     </>
--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 interface BlockProps {
   value?: string | null;
+  onClick?: () => void;
 }
 
 const Block: React.FC<BlockProps> = (props) => {
   return (
     <>
-      <div className="border border-black h-20 w-20 cursor-pointer flex justify-center items-center text-5xl">
+      <div
+        onClick={props.onClick}
+        className="border border-black h-20 w-20 cursor-pointer flex justify-center items-center text-5xl"
+      >
         {props.value}
       </div>
     </>

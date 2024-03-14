1438fd5a31a5342e980ac939d2e898630868b076
Deepak Mardi <deepakmardi22@gmail.com>
2024-03-14 18:53:07 +0530

    Array state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import Block from "./components/Block";
 function App() {
+  const [state, setState] = useState(Array(9).fill(null));
+  console.log("state", state);
   return (
     <>
-      <div className="flex">
-        <div className="ROW ">
+      <div className="p-20 ">
+        <div className="flex">
+          <Block value="x" />
+          <Block value="o" />
+          <Block value="x" />
+        </div>
+        <div className="flex">
           <Block />
           <Block />
           <Block />
         </div>
-        <div className="ROW">
-          <Block />
-          <Block />
-          <Block />
-        </div>
-        <div className="ROW">
+        <div className="flex">
           <Block />
           <Block />
           <Block />
--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-const Block: React.FC = () => {
+interface BlockProps {
+  value?: string | null;
+}
+
+const Block: React.FC<BlockProps> = (props) => {
   return (
     <>
-      <div className="border border-black h-20 w-20"></div>
+      <div className="border border-black h-20 w-20 cursor-pointer flex justify-center items-center text-5xl">
+        {props.value}
+      </div>
     </>
   );
 };

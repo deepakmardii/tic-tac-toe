6cfcdbd03f95ab69ecbfbd66f9974f671e0ff28f
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-10 21:59:31 +0530

    updated

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,8 +1,16 @@
 interface blockProps {
   value: string | null;
+  onClick: () => void;
 }
 const Block: React.FC<blockProps> = (props) => {
-  return <div className="h-20 w-20 border border-black"></div>;
+  return (
+    <div
+      onClick={props.onClick}
+      className="flex cursor-pointer justify-center items-center text-4xl font-medium h-20 w-20 border border-black"
+    >
+      {props.value}
+    </div>
+  );
 };
 
 export default Block;
